bca60a235f49860382fc0ea6b696585457a93d14
haltosan <ethanhsimmons@gmail.com>
2022-03-01 00:07:54 -0700

    end of day push i want to sleep nothing work :|

--- src/main/java/Handler/ClearHandler.java
+++ src/main/java/Handler/ClearHandler.java
@@ -11,7 +11,10 @@ import com.google.gson.GsonBuilder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 
 public class ClearHandler implements HttpHandler {
@@ -27,30 +30,31 @@ public class ClearHandler implements HttpHandler {
         boolean success = false;
         Gson gson = new Gson();
 
-        try{
-            if(exchange.getRequestMethod().equalsIgnoreCase("post")){
-                InputStream body = exchange.getRequestBody();
-                String requestData = readString(body);
-                System.out.println("-- /clear --");
-                System.out.println(requestData);
-                ClearRequest request = gson.fromJson(requestData, ClearRequest.class);
-                ClearService service = new ClearService(request, connection);
-                service.execute();
-                ClearResult result = service.getResult();
-                if(!result.success){
-                    System.out.println("Fail");
-                }
+        if(exchange.getRequestMethod().equalsIgnoreCase("post")){
+            InputStream body = exchange.getRequestBody();
+            String requestData = readString(body);
+            System.out.println("-- /clear --");
+            ClearRequest request = gson.fromJson(requestData, ClearRequest.class);
+            ClearService service = new ClearService(request, connection);
+            service.execute();
+            System.out.println("what is wrong with you child");
+            ClearResult result = service.getResult();
+            System.out.println(result.message);
+            System.out.println(result.success);
+            if(!result.success){
+                System.out.println("Fail"); //todo: fail
             }
-            else{
-                System.out.println("Fail");
-            }
-
+            System.out.println("why are we here...");
+            OutputStream responseBody = exchange.getResponseBody();
+            String json = gson.toJson(result);
+            byte[] jsonBytes = json.getBytes(StandardCharsets.UTF_8); //terrible way of doing this but i need to sleep
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, jsonBytes.length);
+            responseBody.write(jsonBytes);
+            responseBody.close();
+            System.out.println("Response sent");
         }
-        catch (IOException exception){
-            exception.printStackTrace();
-
-            exchange.sendResponseHeaders(HttpURLConnection.HTTP_INTERNAL_ERROR, 0);
-            exchange.getRequestBody().close();
+        else{
+            System.out.println("Fail"); //todo: fail
         }
     }
 
--- src/main/java/Handler/FileHandler.java
+++ src/main/java/Handler/FileHandler.java
@@ -3,11 +3,54 @@ package Handler;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.nio.file.Files;
 
 public class FileHandler implements HttpHandler {
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    public void handle(HttpExchange exchange) throws IOException{
+        if(!exchange.getRequestMethod().equalsIgnoreCase("get")){
+            System.out.println("404 on method");
+            error404(exchange);
+        }
 
+        String path = "web" + exchange.getRequestURI().toString();
+        if(path.equals("web/")){
+            path = "web/index.html";
+        }
+        File file = new File(path);
+        if(!file.exists()){
+            System.out.println("404 on file not existing");
+            error404(exchange);
+        }
+
+        try {
+            exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+        }
+        catch (IOException exception){
+            exception.printStackTrace();
+            error404(exchange);
+        }
+        OutputStream outputStream = exchange.getResponseBody();
+        try {
+            Files.copy(file.toPath(), outputStream);
+            outputStream.close();
+        }
+        catch (IOException exception){
+            exception.printStackTrace();
+            error404(exchange);
+        }
+        System.out.println(path + " served");
+    }
+
+    private void error404(HttpExchange exchange) throws IOException{
+        exchange.sendResponseHeaders(HttpURLConnection.HTTP_INTERNAL_ERROR, 0);
+        File errorPage = new File("/web/HTML/404.html");
+        OutputStream outputStream = exchange.getResponseBody();
+        Files.copy(errorPage.toPath(), outputStream);
+        outputStream.close();
     }
 }
