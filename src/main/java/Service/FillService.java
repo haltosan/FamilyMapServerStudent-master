e7d808b91c2f4ec6a2191e9ac67c5a02617690d9
haltosan <ethanhsimmons@gmail.com>
2022-03-03 17:45:57 -0700

    got tree traversal for fill

--- src/main/java/Service/FillService.java
+++ src/main/java/Service/FillService.java
@@ -7,30 +7,31 @@ import DataAccess.UserDAO;
 import Model.Event;
 import Model.Person;
 import Model.User;
-import Request.FillRequest;
 import Result.FillResult;
 
 import java.sql.Connection;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Fills the database with data
  */
 public class FillService extends Service{
 
-    private final FillRequest request;
-    private final Connection connection;
     private final String username;
     private FillResult result;
     private final int generationCount;
-    private Random random;
+    private final Random random;
+    EventDAO eventDAO;
+    PersonDAO personDAO;
+    UserDAO userDAO;
 
-    public FillService(FillRequest request, Connection connection, String username, int generationCount) {
-        this.request = request;
-        this.connection = connection;
+    public FillService(Connection connection, String username, int generationCount) {
         this.username = username;
         this.generationCount = generationCount;
         random = new Random();
+        eventDAO  = new EventDAO(connection);
+        personDAO = new PersonDAO(connection);
+        userDAO = new UserDAO(connection);
     }
 
     private String[] names = {"billy", "bob", "joe", "fred"};
@@ -38,16 +39,13 @@ public class FillService extends Service{
     private double[][] locations = {{1,1},{2,2},{100, -100}};
     private String[] country = {"us", "us", "ireland", "ireland"};
     private String[] city = {"new york", "la", "on the street", "random pub"};
+    private String[] genders = {"m","f"};
 
     /**
      * Fills the database as indicated in the request
      */
     @Override
     public void execute() {
-        EventDAO eventDAO  = new EventDAO(connection);
-        PersonDAO personDAO = new PersonDAO(connection);
-        UserDAO userDAO = new UserDAO(connection);
-
         //make user's person obj and birth event
         User user;
         try {
@@ -58,27 +56,90 @@ public class FillService extends Service{
             return;
         }
 
+        Person userPerson = new Person(user.getPersonID(), username, user.getFirstName(), user.getLastName(), user.getGender(), null, null, null);
+        int userBirthYear = 2000;
+        //todo: birth for user
+
+        //breadth first tree traversal! (I like these)
+        Queue<Person> people = new ArrayDeque<>();
+        Queue<Integer> birthYear = new ArrayDeque<>();
+        people.add(userPerson);
+        birthYear.add(userBirthYear);
+        Queue<Person> donePeople = new ArrayDeque<>();
+        int currentGeneration = 0;
+        int peoplePerGeneration = 0;
+        while(currentGeneration >= generationCount){
+            int childBirthYear = birthYear.remove();
+            Person child = people.remove();
+            peoplePerGeneration++;
+
+            Person mother = newPerson("f");
+            Person father = newPerson("m");
+            int marriageDate = childBirthYear - 20;
+            double[] marriageLatLong = newLongLat();
+            String[] marriageLocation = newCountryCity();
+
+            Person[] parents = {mother, father};
+            for (Person parent : parents) {
+                int bDay = childBirthYear - 21;
+                try {
+                    makeBirth(parent.getPersonID(), bDay); //todo: add variation to dates?
+                    //Mawage. Mawage is wot bwings us togeder tooday. Mawage, that bwessed awangment, that dweam wifin a dweam… And wuv, tru wuv, will fowow you foweva… So tweasure your wuv.
+                    eventDAO.insert(new Event("event", username, parent.getPersonID(), marriageLatLong[0], marriageLatLong[1], marriageLocation[0], marriageLocation[1], "marriage", marriageDate));
+                    makeDeath(parent.getPersonID(), bDay + 87);
+                    birthYear.add(bDay);
+                } catch (DataAccessException exception) {
+                    exception.printStackTrace();
+                    result = new FillResult("Failed when creating events for " + parent.getPersonID(), false);
+                    return;
+                }
+            }
+            //now the paperwork is being signed
+            mother.setSpouseID(father.getPersonID());
+            father.setSpouseID(mother.getPersonID());
+            //the miracle of birth!
+            child.setMotherID(mother.getPersonID());
+            child.setFatherID(father.getPersonID());
+            donePeople.add(child); //make sure we add child to db later
+
+            people.add(mother);
+            people.add(father);
+
+            if(peoplePerGeneration >= Math.pow(2, currentGeneration)){
+                currentGeneration++;
+                peoplePerGeneration = 0;
+            }
+        }
+
+        //for person in donePeople : add to db
+
         try {
-            personDAO.insert(new Person(user.getPersonID(), username, user.getFirstName(), user.getLastName(), user.getGender(), null, null, null));
+            personDAO.insert(userPerson);
         } catch (DataAccessException exception) {
             exception.printStackTrace();
             result = new FillResult("Creating user's person faulted", false);
             return;
         }
-
-
+        result = new FillResult("this has worked", true);
     }
 
-    private void makeBirth(String username, String personID, int birthYear){
-        double[] location = newLongLat();
-        String[] countryCity = newCountryCity();
-        new Event("event" + Nonce.next(),username,personID,location[0],location[1],countryCity[0],countryCity[1],"birth",birthYear);
+    public FillResult getResult() {
+        return result;
     }
 
-    private void makeDeath(String username, String personID, int birthYear){
+    private void makeEvent(String personID, int year, String type) throws DataAccessException {
         double[] location = newLongLat();
         String[] countryCity = newCountryCity();
-        new Event("event" + Nonce.next(),username,personID,location[0],location[1],countryCity[0],countryCity[1],"death",birthYear + 49);
+        if(type == null){
+            type = newType();
+        }
+        eventDAO.insert(new Event("event" + Nonce.next(),username,personID,location[0],location[1],countryCity[0],countryCity[1],type,year));
+    }
+    private void makeBirth(String personID, int birthYear) throws DataAccessException {
+        makeEvent(personID, birthYear, "birth");
+    }
+    private void makeDeath(String personID, int birthYear) throws DataAccessException {
+        makeEvent(personID, birthYear + 49, "death");
     }
 
     private String newName(){
@@ -93,6 +154,10 @@ public class FillService extends Service{
     private String[] newCountryCity(){
         int index = random.nextInt(country.length);
         return new String[]{country[index], city[index]};
+    }
+
+    private Person newPerson(String gender){
+        return new Person("person" + Nonce.next(),username,newName(),newName(),gender,null,null,null);
     }
 }
 
